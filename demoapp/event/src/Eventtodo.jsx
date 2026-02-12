9345307a38089c0ebaf0272e0e9a8c0a8202f53b
saumya-st <saumyacodes02@gmail.com>
2026-02-12 14:48:58 +0530

    updated code

--- demoapp/event/src/Eventtodo.jsx
+++ demoapp/event/src/Eventtodo.jsx
@@ -1,12 +1,40 @@
-export default function Eventtdo()
-{ function handleclick(){
-    alert("You pressed button")
+export default function Eventtdo() {
+  function handleclick(name) {
+    alert(`Button pressed by ${name}`);
+  }
+  function handleSubmit(e)
+  {
+e.preventDefault();
+const input = e.target.value;
+const inputValue =input.trim();
+if (inputValue==="")
+{
+    alert("Task can not be empty");
+    return;
 }
-    return(
-        <>
-<h1>Event handling</h1>
-<button onClick={handleclick}>Click me</button> //no round brackets needed!
+const li = document.createElement("li");
+<li.contains()
 
-        </>
-    );
+  }
+
+  return (
+    <>
+      <h1>to do app</h1>
+      <button onClick={() => handleclick('Kiet')}>
+        Click me
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input type = "text" name = "task" placeholder="Add your task here"/>
+        <input type="submit">Add task</input>
+        <form>
+            <ul id="tasklist" onClick={handleClickList}>
+
+
+            </ul>
+        </form>
+
+
+      </form>
+    </>
+  );
 }
